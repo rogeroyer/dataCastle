4d5342b5f357c54841982dec7b912b1469182f39
roger <rogerluo387@gmail.com>
2017-10-25 14:41:53 +0800

    Create xgb.py

---
+++ o2o/XGBOOST/xgb.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+import pandas as pd
+
+# import xgboost as xgb
+# data = np.random.rand(5, 10)  # 5 entities, each contains 10 features
+# label = np.random.randint(2, size=5)  # binary target
+# dtrain = xgb.DMatrix(data, label=label)
+# dtest = dtrain
+# param = {'bst:max_depth': 2, 'bst:eta': 1, 'silent': 1, 'objective': 'binary:logistic'}
+# param['nthread'] = 4
+# param['eval_metric'] = 'auc'
+# evallist = [(dtest, 'eval'), (dtrain, 'train')]
+# num_round = 10
+# bst = xgb.train(param, dtrain, num_round, evallist)
