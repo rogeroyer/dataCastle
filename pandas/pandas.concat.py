1730e77ea57716fa4c753beab1acd644271a3ba3
roger <rogerluo387@gmail.com>
2018-05-10 22:51:17 +0800

    Create pandas.concat.py

---
+++ pandas/pandas.concat.py
@@ -0,0 +1,33 @@
+>>> s1 = pd.Series(['a', 'b'])
+>>> s2 = pd.Series(['c', 'd'])
+>>> pd.concat([s1, s2])
+0    a
+1    b
+0    c
+1    d
+dtype: object
+
+
+>>> df1 = pd.DataFrame([['a', 1], ['b', 2]],
+...                    columns=['letter', 'number'])
+>>> df1
+  letter  number
+0      a       1
+1      b       2
+>>> df2 = pd.DataFrame([['c', 3], ['d', 4]],
+...                    columns=['letter', 'number'])
+>>> df2
+  letter  number
+0      c       3
+1      d       4
+>>> pd.concat([df1, df2])
+  letter  number
+0      a       1
+1      b       2
+0      c       3
+1      d       4
+>>> pd.concat([df1, df2], axis=1)
+  letter  number letter  number
+0      a       1      c       3
+1      b       2      d       4
+
